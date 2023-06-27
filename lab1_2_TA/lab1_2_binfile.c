a52c538cc15a3b5db95c2d76f405a89f5ab891ac
Yevhenii Kosolapov <94376914+ka3ax9@users.noreply.github.com>
2023-06-27 11:55:29 +0300

    Update lab1_2_binfile.c

--- lab1_2_TA/lab1_2_binfile.c
+++ lab1_2_TA/lab1_2_binfile.c
@@ -14,7 +14,7 @@ void createBinaryFile(const char* filename, int* numbers, int numNumbers) {
 }
 
 int main() {
-    const char* filename = "numbers.bin";
+    const char* filename = "input.bin";
     int numbers[] = { 11, 22, 33, 44, 55, 66, 77, 88, 99, 110, 121, 132 };
     int numNumbers = sizeof(numbers) / sizeof(numbers[0]);
 
